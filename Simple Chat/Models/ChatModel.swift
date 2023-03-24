af7ef1ad8eea5246f4be917f9fd6a484c50fa684
Nikolomoec <nikolomoec@gmail.com>
2023-03-24 08:28:58 +0200

    - Add ChatModel, where I add Chat and ChatMessage Models

---
+++ Simple Chat/Models/ChatModel.swift
@@ -0,0 +1,39 @@
+//
+//  chatModel.swift
+//  Simple Chat
+//
+//  Created by Nikita Kolomoec on 24.03.2023.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct Chat: Codable {
+    
+    @DocumentID var id: String?
+    
+    var lastmsg: String?
+    
+    var numchats: String
+    
+    @ServerTimestamp var updated: Date?
+    
+    var chats: [String]
+    
+    var msgs: [ChatMessage]?
+    
+}
+
+struct ChatMessage: Codable {
+    
+    @DocumentID var id: String?
+    
+    var imageurl: String?
+    
+    var msg: String
+    
+    @ServerTimestamp var timestamp: Date?
+    
+    var senderId: String
+}
